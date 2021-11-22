8567693e7ddecab59794728a94be5567c5274cca
krishnaKapadia <krishnakapadia@live.com>
2021-11-22 14:46:12 +1300

    Feat: GET alert/s

--- src/entities/alert/index.ts
+++ src/entities/alert/index.ts
@@ -1,13 +1,27 @@
 /** @format */
+import { GlobalConfig } from "../../config";
+import Network from "../../network";
 import { IAlert } from "./models";
 
 export class Alerts {
-  baseUrl: string = "alerts/";
+  baseUrl: string = "alerts";
 
-  get(id?: string): IAlert | IAlert[] {
-    return {
-      id: "1",
-      name: "Test",
-    };
+  /**
+   * Retrieves alerts for the current user
+   * @param identifier - Id related to a specific alert
+   * @returns A single alert or an array of alerts
+   */
+  async get(identifier?: string): Promise<IAlert> {
+    let urlSegments = [];
+
+    if (identifier) {
+      urlSegments = [GlobalConfig.planId, this.baseUrl, identifier];
+    } else {
+      urlSegments = ["user", GlobalConfig.userId, this.baseUrl, GlobalConfig.planId];
+    }
+
+    const url = Network.buildApiUrl(urlSegments);
+    const res = await Network.get<IAlert>(url);
+    return res;
   }
 }
